24b7eec660ae8c037fa698d5c54a940c216cb294
zjsduba <1410453397@qq.com>
2021-02-08 10:23:44 +0800

    1.init

--- src/main/java/com/example/springaop/ext/ExtConfig.java
+++ src/main/java/com/example/springaop/ext/ExtConfig.java
@@ -41,6 +41,35 @@ import org.springframework.context.annotation.Configuration;
  *      4.自定义发布事件
  *          ApplicationContext.publishEvent(Object object)
  *      原理：
+ *          1.ContextRefreshedEvent事件：
+ *              1.容器创建对象，refresh();
+ *              2.finishRefresh();容器刷新完成
+ *              3.publishEvent(new ContextRefreshedEvent(this));
+ *                  1.容器发布事件：ContextRefreshedEvent
+ *                  2.自己发布事件
+ *              4.容器关闭会发布ContextClosedEvent事件
+ *                  事件发布流程：
+ *                      1.获取事件的多播器（派发器），getApplicationEventMulticaster()
+ *                      2.multicastEvent派发事件
+ *                      3.获取到所有的applicationListener
+ *                          for (ApplicationListener<?> listener : getApplicationListeners(event, type)) {
+ *                          1.如果有Executor，可以支持使用Executor进行异步派发
+ *                              Executor executor=getTaskExecutor();
+ *                          2.否则，同步的方式直接执行invokeListener(listener,event);
+ *                          拿到listener回调onApplicationEvent方法
+ *          2.[事件多播器(派发器)]
+ *              1.容器创建对象，refresh()方法；
+ *              2.initApplicationEventMulticaster();初始化ApplicationEventMulticaster
+ *                  1.先去容器中找有没有ID="applicationEventMulticaster"的组件；
+ *                  如果没有，SimpleApplicationEventMulticaster simpleApplicationEventMulticaster = new SimpleApplicationEventMulticaster(beanFactory)
+ *                  ，加入到容器中，我们就可以在其他组件要派发事件，自动注入这个applicationEventMulticaster
+ *          3.容器中有哪些监听器
+ *              1.容器创建对象，refresh()方法；
+ *              2.registerListeners();
+ *                  从容器中拿到所有的监听器，把他们注册到applicationEventMulticaster中
+ *                  String[] listenerBeanNames = getBeanNamesForType(ApplicationListener.class, true, false);
+ *              3.将listener注册到applicationEventMulticaster
+ *              getApplicationEventMulticaster().addApplicationListenerBean(listenerBeanName);
  *
  * @date 2021/2/3 9:07
  * @version 1.0
